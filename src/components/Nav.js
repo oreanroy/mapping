c618c5505b923b0df07d368691f23dc313864e14
oreanroy <oreanroy@gmail.com>
2018-11-05 19:15:30 +0530

    click func to be added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,47 @@
 import React, {Component} from "react";
 
 export default class Nav extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            navstyle: "0",
+            query: "",
+            showing: []
+            }
+        }
+    
+    openNav = () =>{
+        //document.getElementById("Sidenav").style.width = "250px";
+        this.setState({navstyle:  "250px" })
+    }
+    
+    closeNav = ()  =>{
+        //document.getElementById("Sidenav").style.width = "0";
+        this.setState({navstyle:  "0" })
+    }
+    filter = (query) =>{
+        var filtered = [];
+        if(query ===''){
+           // this.setState({shown: this.state.places})
+           //set the shown to all
+           this.props.changeMarker(this.props.places.places)
+        }
+        if(query !=='' && isNaN(query)){
+            filtered = this.props.places.places.filter(place => place.name.toLowerCase().includes(query.toLowerCase()));
+            this.props.changeMarker(filtered)
+        }
+    }
     render() {
+        var style = {
+            size: "30px",
+            cursor: "pointer"
+        }
+        var stylenav = {
+            width: this.state.navstyle
+        }
+        //console.log(this.props)
         return(
-            <div>
+            /*<div>
                 <h3>Explore</h3>
                 <select id="places">
                     <option value="cafe">Cafe</option>
@@ -12,6 +50,17 @@ export default class Nav extends Component {
                     <option value="hospital">Hospitals</option>
                 </select>
 
+            </div> */
+            <div>
+            <div id="Sidenav" className="nav" style={stylenav}>
+                <a href={void(0)} className="closebtn" onClick={this.closeNav}>&times;</a>
+                <input className="input" onChange={event=>this.filter(event.target.value)} />
+                <ul>
+
+                {this.props.renderPlaces.map((place) => <li onClick = {this.props.onMarkerClick}>{place.name}</li>)}
+                </ul>
+            </div>
+            <span style={style} onClick={this.openNav}>&#9776; Explore</span>
             </div>
         )
     }
